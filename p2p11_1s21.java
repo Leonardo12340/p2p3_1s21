6d3cf84fc6f1aa332860f1199e80df65e3e7d1d6
Leonardo12340 <144502307+Leonardo12340@users.noreply.github.com>
2023-10-24 15:01:19 -0600

    Add files via upload

---
+++ p2p11_1s21.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class P2p10{
+	public static void main(String[] args) {
+		for(String cadena : args){
+			char operacion = cadena.charAt(1);
+			swhtich(operacion){
+		case 'a': System.out.println("incisi a")
+		case 'b': System.out.println("inciso b")
+		case 'h': System.out.println("inciso h")
+		case 'e': System.out.println("inciso e ")		
